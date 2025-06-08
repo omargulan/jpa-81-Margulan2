98500a087a0e36c98f1693c30ca20eddae3dc514
runtime-1 <runtimestudent2023@gmail.com>
2025-06-08 16:53:03 +0500

    08-06-2025 Mock

--- src/test/java/com/practice/springjpa81/service/ProductServiceTest.java
+++ src/test/java/com/practice/springjpa81/service/ProductServiceTest.java
@@ -4,10 +4,14 @@ import com.practice.springjpa81.model.Category;
 import com.practice.springjpa81.model.Product;
 import com.practice.springjpa81.repository.CategoryRepository;
 import com.practice.springjpa81.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ProductServiceTest {
     // TODO: написать два теста
@@ -17,22 +21,46 @@ public class ProductServiceTest {
     @Test
     void create_shouldThrowException_WhenCategoryIdExist(){
         CategoryRepository categoryRepository = Mockito.mock(CategoryRepository.class);
-        CategoryService categoryService = new CategoryService(categoryRepository);
         ProductRepository productRepository = Mockito.mock(ProductRepository.class);
-        ProductService productService = new ProductService(productRepository);
+        ProductService productService = new ProductService(categoryRepository, productRepository);
 
         Category existingCategory = new Category();
         existingCategory.setId(1L);
 
         Product product = new Product();
         product.setName("Клавиатура");
+        product.setPrice(15000.00);
+        product.setId(100L);
+        //product.setCategory(existingCategory);
 
         Mockito.when(categoryRepository.findById(existingCategory.getId()))
                 .thenReturn(Optional.of(existingCategory));
         Mockito.when(productRepository.save(product))
                 .thenReturn(product);
 
-        Product productCreated = prod
+        Product productCreated = productService.create(product, existingCategory.getId());
+        assertEquals(existingCategory.getId(), productCreated.getCategory().getId());
+
+    }
+
+    @Test
+    void create_shouldThrowException_WhenCategoryIsNotExist(){
+        CategoryRepository categoryRepository = Mockito.mock(CategoryRepository.class);
+        ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+
+        ProductService productService = new ProductService(categoryRepository, productRepository);
+
+
+        Product product = new Product();
+        product.setName("Клавиатура");
+        product.setPrice(15000.00);
+        //product.setId(100L);
+
+        long categoryId=1L;
+
+        RuntimeException ex = Assertions.assertThrows(RuntimeException.class, () -> productService.create(product, categoryId));
+        assertEquals("Категория не найдена", ex.getMessage());
+
 
     }
 }
